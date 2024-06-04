corrected loss computation

fluospotter/io.py
@@ -1,6 +1,6 @@
 """Dataset preparation functions."""
 
-from typing import List, Tuple, Union, Dict
+from typing import List, Tuple, Union, Dict, Any
 import os
 from tifffile import imread
 
@@ -9,6 +9,14 @@ EXTENSIONS = ("tif", "tiff")
 
 
 def check_configuration_file(cfg: Dict) -> Dict:
+    """Ensures the configuration dictionary contains all necessary parameters.
+
+        Args:
+            cfg: Configuration dictionary.
+
+        Returns:
+            Updated configuration dictionary with default values added for missing parameters.
+    """
     params = [
         "n_classes", "model_name", "pretrained", "loss1", "loss2",
         "alpha1", "alpha2", "batch_size", "acc_grad", "n_samples", "neg_samples",
@@ -65,6 +73,17 @@ def load_files(fname: str, training: bool = False) -> Dict[str, List[str]]:
 
 
 def load_folder(fname: str) -> List[str]:
+    """Loads all image files from a given folder and checks their consistency in size.
+
+        Args:
+            fname: Path to the folder containing image files.
+
+        Returns:
+            A list of file paths.
+
+        Raises:
+            ValueError: If the files do not coincide in shape or cannot be loaded.
+    """
     files = [os.path.join(fname, file) for file in os.listdir(fname) if file.lower().endswith(EXTENSIONS)]
     size = None
     for f in files:
@@ -80,6 +99,15 @@ def load_folder(fname: str) -> List[str]:
 
 
 def validate_data_and_labels(data: Dict[str, List[str]], labels: Dict[str, List[str]]) -> bool:
+    """Validates that data and label files match in their basename (excluding extensions).
+
+        Args:
+            data: Dictionary containing lists of data file paths.
+            labels: Dictionary containing lists of label file paths.
+
+        Returns:
+            True if data and label files match, False otherwise.
+    """
     for key in ['train', 'valid', 'test']:
         if remove_extension(data[key]) != remove_extension(labels[key]):
             return False
@@ -87,8 +115,15 @@ def validate_data_and_labels(data: Dict[str, List[str]], labels: Dict[str, List[
 
 
 def remove_extension(file_list):
-    return {os.path.basename(os.path.splitext(file)[0]) for file in file_list}
+    """Removes the file extension from a list of file paths.
 
+        Args:
+            file_list: List of file paths.
+
+        Returns:
+            A set of file basenames without extensions.
+    """
+    return {os.path.basename(os.path.splitext(file)[0]) for file in file_list}
 
 '''
 def load_model(fname: Union[str, "os.PathLike[str]"]) -> tf.keras.models.Model:
@@ -128,5 +163,4 @@ def load_prediction(fname: Union[str, "os.PathLike[str]"]) -> pd.DataFrame:
     if not all([c in df.columns for c in ["x [px]", "y [px]"]]):
         raise ValueError("Prediction file must contain columns 'x [px]' and 'y [px]'.")
     return df
-
 '''

fluospotter/losses.py
@@ -21,15 +21,14 @@ class CompoundLoss(nn.Module):
         self.alpha2 = alpha2
 
     def forward(self, y_pred, y_true, y_true_sk=None):
-        #pdb.set_trace()
         if y_true_sk:
-            l1 = self.loss1(y_pred[0,0:1], y_true[0,0:1], y_true_sk)
+            l1 = self.loss1(y_pred[0,1], y_true[0,1], y_true_sk)
             l2 = self.loss2(y_pred[0,2:], y_true[0,2:], y_true_sk)
         elif y_pred.shape[1] > 2:
-            l1 = self.loss1(y_pred[0,0:1], y_true[0,0:1])
+            l1 = self.loss1(y_pred[0,1], y_true[0,1])
             l2 = self.loss2(y_pred[0,2:], y_true[0,2:])
         else:
-            l1 = self.loss1(y_pred[0,0], y_true[0,0])
+            l1 = self.loss1(y_pred[0,1], y_true[0,1])
             l2 = self.loss2(y_pred[0,1], y_true[0,1])
         return self.alpha1*l1 + self.alpha2 * l2
 

fluospotter/metrics.py
@@ -6,10 +6,16 @@ import warnings
 import numpy as np
 import pandas as pd
 import scipy.optimize
-import numpy as np
 from scipy.stats import rankdata
+from typing import Dict, Any
+from datasets import Dataset
+from models import Model
 
 EPS = 1e-12
+
+
+def compute_segmentation_metrics(model: Model, cfg: Dict, data: Dataset):
+    return True
 
 
 def fast_bin_auc(actual, predicted, partial=False):

fluospotter/models/nuclei.py
@@ -1,6 +1,5 @@
 """segmentation class."""
 
-import functools
 import numpy as np
 import torch
 from ..datasets import Dataset
@@ -8,13 +7,15 @@ from ..losses import combined_f1_rmse, f1_score, rmse
 from ._models import Model
 from ..networks.unet import CustomUNet
 from ..training import train_model
-import os, os.path
 from ..io import check_configuration_file
+from ..metrics import compute_segmentation_metrics
+
 
 class SegmentationModel(Model):
     """Class to segment nuclei; see base class."""
 
-    def __init__(self, pretrained=None, model_name='small_unet_3d', in_channels=1, n_classes=2, patch_size=(64, 64, 16), configuration={}, **kwargs):
+    def __init__(self, pretrained=None, model_name='small_unet_3d', in_channels=1, n_classes=2, patch_size=(64, 64, 16),
+                 configuration={}, **kwargs):
         super().__init__(**kwargs)
         self.network = CustomUNet(model_name=model_name, pretrained=pretrained, in_c=in_channels, n_classes=n_classes,
                                   patch_size=patch_size).model
@@ -31,12 +32,12 @@ class SegmentationModel(Model):
         ]
 
     def train(
-        self, dataset: Dataset, augment_val: bool = True, callbacks: list = None,
+            self, dataset: Dataset, augment_val: bool = True, callbacks: list = None,
     ) -> None:
         if not dataset.training:
             raise ValueError('Dataset does not contain training data.')
 
-        train_model(dataset=dataset, model=self)
+        train_model(dataset=dataset, model=self, model_type="segmentation")
 
     def predict(self, dataset: Dataset) -> None:
         test_sequence = dataset.segmentation_data_test()
@@ -44,3 +45,6 @@ class SegmentationModel(Model):
     def predict_on_image(self, image: np.ndarray) -> np.ndarray:
         """Predict on a single input image."""
         return self.network.predict(image[None, ..., None], batch_size=1).squeeze()
+
+    def evaluate(self, dataset: Dataset, display: bool = True) -> None:
+        compute_segmentation_metrics(model=self.network, cfg=self.cfg, data=dataset)

fluospotter/training.py
@@ -43,13 +43,11 @@ def train_one_epoch(model, tr_loader, bs, acc_grad, loss_fn, optimizer, schedule
     with trange(len(tr_loader)) as t:
         step, n_elems, running_loss = 0, 0, 0
         for (i_batch, batch_data) in enumerate(tr_loader):  # load 1 scan from the training set
-            #if i_batch == 2: break
             n_samples = len(batch_data['seg'])  # nr of px x py x pz patches (see args.n_samples)
             for m in range(0, n_samples, bs):  # we loop over batch_data picking up bs patches at a time
                 step += bs
                 inputs, labels = (batch_data['img'][m:(m+bs)].to(device), batch_data['seg'][m:(m+bs)].to(device))
                 outputs = model(inputs)
-                #pdb.set_trace()
                 loss = loss_fn(outputs, labels)
                 loss = loss / acc_grad
                 loss.backward()
@@ -80,17 +78,15 @@ def validate(model, loader, loss_fn, slwin_bs=2):
         for val_data in loader:
             images, labels = val_data["img"].to(device), val_data["seg"]
             n_classes = labels.shape[1]
-            # val_outputs = sliding_window_inference(val_images.to(device), patch_size, slwin_bs, model, mode='gaussian').cpu()
             preds = sliding_window_inference(images, patch_size, slwin_bs, model, overlap=0.1, mode='gaussian').cpu()
             del images
-
             loss = loss_fn(preds, labels)
             preds = preds.argmax(dim=1).squeeze().numpy()
             labels = labels.squeeze().numpy().astype(np.int8)
 
             dsc_score, auc_score = [], []
             #pdb.set_trace()
-            for l in range(n_classes):
+            for l in range(1,n_classes):
                 dsc_score.append(fast_bin_dice(labels[l], preds == l))
                 auc_score.append(fast_bin_auc(labels[l], preds == l, partial=True))
                 if np.isnan(dsc_score[l]): dsc_score[l] = 0
@@ -172,7 +168,8 @@ def train_segmentation_model(model, optimizer, acc_grad, loss_fn, bs, tr_loader,
 
 def train_model(
     model: Model,
-    dataset: Dataset
+    dataset: Dataset,
+    model_type: str
 ) -> None:
     callbacks = []
     use_cuda = torch.cuda.is_available()
@@ -189,14 +186,14 @@ def train_model(
     else:
         save_path = os.path.join(dataset.data_dir, '../experiments')
 
-    if cfg["model_type"] == "segmentation":
+    if model_type == "segmentation":
         save_path = os.path.join(save_path, 'segmentation')
         labels_path = dataset.segmentation_dir
-    elif cfg["model_type"] == "punctaDetection":
+    elif model_type == "punctaDetection":
         save_path = os.path.join(save_path, 'punctaDetection')
         labels_path = dataset.spots_dir
     else:
-        raise ValueError("Model type {} not recognized".format(cfg["model_type"]))
+        raise ValueError("Model type {} not recognized".format(model_type))
 
     os.makedirs(save_path, exist_ok=True)
 
@@ -209,7 +206,7 @@ def train_model(
     #patch_size = args.patch_size.split('/')
     #patch_size = tuple(map(int, patch_size))
 
-    print('* Instantiating a {} model for {}'.format(model.model_name, cfg["model_type"]))
+    print('* Instantiating a {} model for {}'.format(model.model_name, model_type))
     in_c = 1
     model = model.network
     model = model.to(device)
@@ -244,7 +241,7 @@ def train_model(
     print('* Starting to train\n', '-' * 10)
     start = time.time()
 
-    if cfg["model_type"] == "segmentation":
+    if model_type == "segmentation":
         tr_info = train_segmentation_model(model, optimizer, cfg["acc_grad"], loss_fn, int(cfg["batch_size"]), tr_loader, ovft_loader, vl_loader, scheduler,
                           cfg["metric"], int(cfg["n_epochs"]), int(cfg["vl_interval"]), save_path, int(cfg["n_classes"]) > 1)
     end = time.time()